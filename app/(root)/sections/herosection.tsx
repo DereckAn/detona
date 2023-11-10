b58a63089f4d23ca66f847e724149a5facf0418f
Dereck <dereckangeles15071998@gmail.com>
2023-11-09 18:58:53 -0700

    hero

--- app/(root)/sections/herosection.tsx
+++ app/(root)/sections/herosection.tsx
@@ -3,18 +3,26 @@ import Container from "@/components/ui/container";
 import Image from "next/image";
 
 const HeroSection = () => {
-    return ( 
-        <section className="bg-orange-600 h-[91vh]">
-            <Container>
-            <Image src={asp} alt="hero image" layout="fill" objectFit="cover" objectPosition="center" className=" z-10"/>
-                <div className=" border">
-                        <h1 className="text-2xl">Detona Clean - Your Ultimate Cleaning Solutions</h1>
-                        <p>Professional cleaning services</p>
+  return (
+    <section className="bg-gray-400 h-[91vh]">
+      <Container className="">
+        {/* <Image src={asp} alt="hero image" layout="fill" objectFit="cover" objectPosition="center" className=" z-10"/> */}
+        <div className="border max-w-xs space-y-3 items-center " id="hola">
+          <h1 className="text-3xl font-bold ">
+            Detona Clean - Your Ultimate Cleaning Solutions
+          </h1>
+        <div>
+          <p>
+            At our online vacuum cleaner store, we provide the best cleaning
+            solutions for all your needs. From powerful uprights to versatile
+            canisters, we have a wide selection of high-quality vacuums to
+            choose from.
+          </p>
+        </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
 
-                </div>
-            </Container>
-        </section>
-     );
-}
- 
 export default HeroSection;
--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -9,7 +9,7 @@ const Container = ({
   className?: string;
 }) => {
   return (
-    <div className={cn("container pl-[20vw] py-5 lg:py-8", className)}>
+    <div className={cn("container lg:px-[20vw]  py-5 lg:py-8", className)}>
       {children}
     </div>
   );
